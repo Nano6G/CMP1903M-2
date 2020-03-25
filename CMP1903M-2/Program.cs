da6348eb997f47ef4ef3f17e22d80bac3bc84a25
Will Smith <19693578@students.lincoln.ac.uk>
2020-03-25 12:48:56 +0000

    Added Comments

--- CMP1903M-2/Program.cs
+++ CMP1903M-2/Program.cs
@@ -7,28 +7,35 @@ namespace CMP1903M_2
     {
         static void Main()
         {
-            Console.Write("\nEnter the first text file to compare: ");
+            //User enters the name of the two text files to compare, names are stored in string format
+            Console.Write("\nEnter the first text file to compare (Do not include .txt): ");
             string file1Name = Console.ReadLine();
-            Console.Write("Enter the second text file to compare: ");
+            Console.Write("Enter the second text file to compare (Do not include .txt): ");
             string file2Name = Console.ReadLine();
 
+            //Input is displayed to the user
             Console.Write("[Input] diff ");
             Console.Write(file1Name);
             Console.Write(".txt ");
             Console.Write(file2Name);
             Console.Write(".txt\n");
 
+            //Gets the current directory
             string directory = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
             string path = Path.GetFullPath(Path.Combine(directory, @"..\"));
 
+            //Try/Catch to handle if the user enters an invalid text file
             try
             {
+                //Stores the two text files
                 System.IO.StreamReader file1 = File.OpenText(path + file1Name + ".txt");
                 System.IO.StreamReader file2 = File.OpenText(path + file2Name + ".txt");
 
+                //Reads through both text files and stores contents to string
                 string file1Text = file1.ReadToEnd();
                 string file2Text = file2.ReadToEnd();
 
+                //Determines if the files contents are the same or not and displays result to the user
                 if (file1Text == file2Text)
                 {
                     Console.Write("[Output] ");
@@ -48,10 +55,12 @@ namespace CMP1903M_2
             }
             catch
             {
+                //Displays error to the user and restarts process
                 Console.WriteLine("Please enter the name of a valid text file in the current directory");
                 Main();
             }
 
+            //Restarts process
             Main();
             
 
